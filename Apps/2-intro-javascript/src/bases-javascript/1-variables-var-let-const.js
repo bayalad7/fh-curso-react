abfb0cf90b48489119d8ceef64e02ba3d6ceb22b
Benjamín Ayala <bayala0@ucol.mx>
2022-09-20 19:36:14 -0500

    Seccion 2: Las variables var, let y const

---
+++ Apps/2-intro-javascript/src/bases-javascript/1-variables-var-let-const.js
@@ -0,0 +1,13 @@
+// Las variables de tipo VAR ya no se deben usar en la actualidad
+// Variables Let y Constantes
+const nombre = "Benjamín";
+const apellido = "Ayala";
+let edad = 30;
+    edad = 31;
+console.log( nombre, apellido, edad );
+if( true )
+{
+    edad = 40
+    console.log( edad );
+}
+console.log( nombre, apellido, edad );
