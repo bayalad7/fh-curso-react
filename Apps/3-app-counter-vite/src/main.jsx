3f7b2d2f161421f2604a5b3f7328f5678902a776
Benjamín Ayala <bayala0@ucol.mx>
2022-12-28 10:38:41 -0600

    Seccion 3: Hola mundo en React

--- Apps/3-app-counter-vite/src/main.jsx
+++ Apps/3-app-counter-vite/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+/**
+ * Notas.
+ * 1.- Al empezar la aplicación de React, necesitamos de un componente funcional (functionals components).
+ *     Esto es lo que se acostumbra hoy en día al trabajar con React, puros componentes basados en funciones.
+ * 2.- Todos los nombres de los componentes funcionales, van capitalizados con Pascal Case o Upper Camel Case.
+ */
+const App = () => {
+    return (<h1>¡Hola Mundo!</h1>);
+}
+
+/**
+ * Vamos a renderizar nuestra aplicación.
+ * La app se tiene que renderizar en un elemento del DOM.
+ */
+ReactDOM.createRoot( document.getElementById("root") ).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
