01501dfef917a030d43028cf2478cc6ea5058912
Benjamín Ayala <bayala0@ucol.mx>
2023-01-06 01:17:48 -0600

    Seccion 3: funciones handleSubtract y handleReset en counter app

--- Apps/3-app-counter-vite/src/components/counter.jsx
+++ Apps/3-app-counter-vite/src/components/counter.jsx
@@ -3,6 +3,22 @@ import PropTypes from "prop-types"
 
 const Counter = ({ value }) => {
 
+    /**
+     * NOTA IMPORTANTE
+     * Cuando cambia el estado del componente en React, el componente se vuelve a renderizar,
+     * en este caso cuando se ejecuta la función handleAdd, handleSubtract o handleReset
+     * veremos como se ejecuta el console.log("render") en la consola del navegador...
+     * 
+     * Y si nosotros tuvieramos alguna petición asíncrona, una petición a una API, un Fetch, Etc..
+     * Cada vez que cambiemos el estado de algún Hook, estas peticiones se volverían a ejecutar en el componente
+     * ya que se esta renderizando nuevamente.
+     * 
+     * Existen Hooks especializados para controlar eso, pero en este caso tenemos que tener presente que si cambiamos
+     * el estado del Hook, se va a volver a disparar el componente.
+     */
+    console.log("render")
+    // PETICIÓN ASÍNCRONA AQUÍ - API - FETCH - ETC...
+
     // Desestructuramos el Hook useState
     /**
      * Notas del Hook useState
@@ -20,17 +36,22 @@ const Counter = ({ value }) => {
      */
     const [ counter , setCounter ] = useState( value )
 
-    const Increment = () => {
+    const handleAdd = () => {
         // Forma 1 de incrementar el valor
         setCounter( counter + value )
-
         // Forma 2 de incrementar el valor, utilizar un nombre de una variable internamente...
-        setCounter( (c) => c + value )
+        // setCounter( (c) => c + value )
     }
+
+    const handleSubtract = () => setCounter( counter - value )
+    const handleReset = () => setCounter( value )
+
     return (
         <>
             <h1>App Counter: { counter }</h1>
-            <button onClick={ Increment } >¡Click {value}++!</button>
+            <button onClick={ handleAdd } >¡Click +{value}!</button>
+            <button onClick={ handleSubtract } >¡Click -{value}!</button>
+            <button onClick={ handleReset } >¡Reset!</button>
         </>
     );
 }
